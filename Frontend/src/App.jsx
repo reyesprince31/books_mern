39edb82a9797a8de16c9ee32553496ec04b1e05e
Prince Reyes <krayzerprince@gmail.com>
2023-09-03 21:25:41 +0800

    frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import { Home, CreateBook, EditBook, DeleteBook, ShowBook } from "./pages";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import CreateBook from "./CreateBooks";
+import ShowBook from "./ShowBook";
+import EditBook from "./EditBook";
+import DeleteBook from "./DeleteBook";
+
+export { Home, CreateBook, ShowBook, EditBook, DeleteBook };
